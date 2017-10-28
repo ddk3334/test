a678d59fd68616f058be9c0fc9b17b045212697d
Dhupendra Singh <ddk3334@gmail.com>
2017-10-28 10:42:22 +0530

    modification done

--- devel_workspace/src/devel/test/DevelTest.java
+++ devel_workspace/src/devel/test/DevelTest.java
@@ -2,6 +2,6 @@ package devel.test;
 
 public class DevelTest {
 	public static void main(String[] args) {
-		System.out.println("Say hi devel....!");
+		System.out.println("Say hi devel, how are you?");
 	}
 }
